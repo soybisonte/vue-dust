96e668191189432376e4e9ecdcf0d478b32e7dc3
Heriberto Rodriguez <erz440@gmail.com>
2018-03-23 13:50:30 -0600

    inital commit, and setup

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# vue-dust
+
+> A webpack boilerplate for front end including es6, stylus, rupture, gsap, axios,
+> MIT license, WIP :)
+
+## Build Setup
+
+``` bash
+# install dependencies
+yarn
+
+# serve with hot reload at localhost:8080
+yarn dev
+
+# build for production with minification
+yarn build
+
+# build for production and view the bundle analyzer report
+yarn build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      responseData: []
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  mounted () {
+  },
+  methods: {
+    async fetchData () {
+      let response = await axios.get('http://jsonplaceholder.typicode.com/posts')
+      this.responseData = response.data
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="hello">
+    <h1>Hola</h1>
+    <h3 class="mobileVisible">I'm in mobile only</h3>
+    <ul class="posts">
+      <li v-for="(item, index) in responseData" :key="index" class="post--item">
+        <h3>{{ item.title }}</h3>
+        <small>{{item.userId}} : {{ item.id }}</small>
+        <p>{{ item.body }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="../styles/HelloWorld.styl" lang="stylus"></style>
