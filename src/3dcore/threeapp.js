a52b45207acc6f8cb83f1d476bb706cc61593d32
Hector Heriberto Rodriguez Martinez <mb82922@Heri.local>
2018-10-14 16:19:06 -0500

    pending three

---
+++ src/3dcore/threeapp.js
@@ -0,0 +1,31 @@
+class Application {
+  constructor() {
+    this.objects = [];
+    this.createScene();
+  }
+
+  createScene() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 2000);
+    this.camera.position.z = 20;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+
+    this.render();
+  }
+
+  render() {
+    requestAnimationFrame(() => {
+      this.render();
+    });
+
+    this.objects.forEach((object) => {
+      object.update();
+    });
+
+    this.renderer.render(this.scene, this.camera);
+  }
+
+}
